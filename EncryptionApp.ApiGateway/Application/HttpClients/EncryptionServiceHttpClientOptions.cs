8e6236d323a7584b8a216b3327c3d22307af4051
Vladyslav Kovalik <vladyslav.kovalik@gelios.company>
2022-04-13 22:56:07 +0300

    Fix problems with docker compose file

---
+++ EncryptionApp.ApiGateway/Application/HttpClients/EncryptionServiceHttpClientOptions.cs
@@ -0,0 +1,9 @@
+﻿using EncryptionApp.ApiGateway.Infrastructure.Options;
+
+namespace EncryptionApp.ApiGateway.Application.HttpClients
+{
+    internal sealed class EncryptionServiceHttpClientOptions : HttpClientsOptions
+    {
+        
+    }
+}
